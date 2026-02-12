641b963b30d49043440590e93ce388fae611838f
ariyapanna <ariyapanna@outlook.com>
2026-02-12 21:40:42 +0700

    feat: Add transaction types enum

---
+++ implementation-nodejs/wallet-service/src/constant.js
@@ -0,0 +1,10 @@
+module.exports = {
+    TransactionType: {
+        Unknown: 0,
+        Misc: 1,
+        TopUp: 2,
+        PaymentParking: 3,
+        PaymentMarketplace: 4,
+        PaymentAdvertisementClick: 5
+    }
+}
